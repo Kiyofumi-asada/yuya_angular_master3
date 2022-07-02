d3c0ff0ed51ac82a7f962020bc4f91ff33c79917
浅田清文 <kiyofumi.a.0122@gmail.com>
2022-07-03 06:53:00 +0900

    :adhesive_bandage:HttpClientModule を追加してエラーを解消

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+//declarations
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDataComponent } from './pokemon-data/pokemon-data.component';
+//imports
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+//providers
 import { PokemonService } from './pokemon.service';
 
 @NgModule({
+  //NOTE:Component追加した時に追加する
   declarations: [AppComponent, PokemonListComponent, PokemonDataComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  //NOTE:angularのmoduleを使いたい時に追加する
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [PokemonService],
   bootstrap: [AppComponent],
 })
